5038d4b2cf481cd3c6fdddb23978f696b0a23b6d
Paulina-source <boakyewaapaulina58@gmail.com>
2024-05-18 21:43:00 +0000

    task2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -7,3 +7,15 @@ function processArray(arr)
 const numbers = [5, 10, 15, 20, 25, 30];
 const result = processArray(numbers);
 console.log(result); 
+
+
+function formatArrayStrings(strings, numbers) {
+  return strings.map((str, i) => (numbers[i] % 2 === 0) ? str.toUpperCase() : str.toLowerCase());
+}
+
+
+const strings = ['red', 'yellow', 'green', 'blue', 'orange'];
+const processedNumbers = processArray(numbers);
+const formattedStrings = formatArrayStrings(strings, processedNumbers);
+console.log(processedNumbers); 
+console.log(formattedStrings); 
